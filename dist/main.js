f90f119331dd2ba1f8b75bee72cbdb6f987a2281
znrm <moradianzaniar@gmail.com>
2018-09-19 06:18:57 -0700

    Add top dropdown for selecting mode

--- dist/main.js
+++ dist/main.js
@@ -471,6 +471,14 @@ const buildUI = () => {
     uiElement.innerText = BUTTONS_RIGHT[i];
     document.getElementById('options-buttons').appendChild(uiElement);
   }
+
+  for (let i = 0; i < BUTTONS_TOP.length; i += 1) {
+    const uiElement = document.createElement('li');
+    uiElement.className = 'options-text';
+    uiElement.id = BUTTONS_TOP[i];
+    uiElement.innerText = BUTTONS_TOP[i];
+    document.getElementById('mode-buttons').appendChild(uiElement);
+  }
 };
 
 const startTutorial = async () => {
--- src/interface/ui_elements.js
+++ src/interface/ui_elements.js
@@ -19,6 +19,14 @@ export const buildUI = () => {
     uiElement.innerText = BUTTONS_RIGHT[i];
     document.getElementById('options-buttons').appendChild(uiElement);
   }
+
+  for (let i = 0; i < BUTTONS_TOP.length; i += 1) {
+    const uiElement = document.createElement('li');
+    uiElement.className = 'options-text';
+    uiElement.id = BUTTONS_TOP[i];
+    uiElement.innerText = BUTTONS_TOP[i];
+    document.getElementById('mode-buttons').appendChild(uiElement);
+  }
 };
 
 export const startTutorial = async () => {
