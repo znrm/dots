7613a5a6aa8872eef571692373f3be593cf42ca5
znrm <zaniarmoradian@gmail.com>
2021-03-14 01:39:23 -0800

    Webpack & eslint update

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "prettier",
+  ],
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 7,
+  },
+  overrides: [
+    {
+      files: ["src/**/*.js"],
+      env: {
+        node: false,
+        browser: true,
+        es6: true,
+      },
+      parserOptions: {
+        sourceType: "module",
+      },
+    },
+  ],
+};
